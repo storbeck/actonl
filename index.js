1f3e1772ed3cbdcd4ae6148ad209cb0df583383f
Geoff Storbeck <storbeck@gmail.com>
2021-06-20 11:33:07 -0400

    we have a web server!

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
 require('dotenv').config()
+require('log-timestamp')
+const http = require('http')
 const API = require('call-of-duty-api')();
-const { print } = require('./utilities/output');
+const { print, html } = require('./utilities/output');
 const { sort_identities } = require('./utilities/process');
 
+let data;
+let sorted;
+
+// Login on server start
 (async () => {
     try {
         await API.login(process.env.ACTIVISION_USERNAME, process.env.ACTIVISION_PASSWORD);
-        const data = await API.getEventFeed();
-        const sorted = sort_identities(data.identities)
-        
-        print(sorted)
+
+        console.log('Logged in to API')
     } catch(error) {
         console.error(error)
     }
 })()
+
+const requestListener = async (req, res) => {
+    // Ignore fancy things such as a favicon
+    if (req.url === '/favicon.ico') return;
+
+
+    // Pull new feed on refresh
+    data = await API.getEventFeed();
+    
+
+    // Process our data
+    sorted = sort_identities(data.identities)
+    console.log(`Received event feed -- ${sorted.online.length} ↑  ||  ${sorted.offline.length} ↓`);
+
+    // Write to web server
+    res.writeHead(200)
+    res.end(print(sorted, res))
+}
+
+const server = http.createServer(requestListener)
+server.listen(process.env.PORT || 8080)
--- utilities/output.js
+++ utilities/output.js
@@ -1,19 +1,28 @@
+let output = 'log';
+
+const log = (line) => {
+    if (output === 'log') {
+        console.log(line)
+    } else {
+        output.write(line + '\n')
+    }
+}
 const print_header = (label, cur, total) => {
-    console.log('===================================')
-    console.log(`${label} (${cur}/${total})`)
-    console.log('===================================')
+    log('===================================')
+    log(`${label} (${cur}/${total})`)
+    log('===================================')
 }
 
 const print_list = (users) => {
     users.forEach(u => {
         // Remove #activision_id after username
-        console.log(u.username.replace(/#.*/,''))
+        log(u.username.replace(/#.*/,''))
     })
 }
 
 const print_divider = () => {
-    console.log('')
-    console.log('')
+    log('')
+    log('')
 }
 const print_online = (online, total) => {
     print_header('online', online.length, total)
@@ -25,7 +34,9 @@ const print_offline = (offline, total) => {
     print_list(offline)
 }
 
-const print = (users) => {
+const print = (users, res=false) => {
+    if (res) output=res;
+
     const total = users.online.length + users.offline.length;
     
     print_online(users.online, total)
